ec3ef609e5fd62bb7e848f3214035a6ad32a8d75
jjwiese03 <janwiesmann03@gmail.com>
2026-06-04 21:47:41 +0200

    Renamed Apps folder to AppWebVersion and updated the index.html file to reflect the changes. This is to avoid confusion with the desktop version of the app and to make it clear that this is the web version of the app.

---
+++ docs/AppWebVersion/src/main.js
@@ -0,0 +1,6 @@
+import discplot from "./discplot.js";
+import Interactions from "./discplotInteractions.js";
+import { buildBoostplot } from "./boostplot.js";
+
+
+console.log("main loaded")
--- docs/Facelift_Discplot (1)/src/main.js
+++
@@ -1,6 +0,0 @@
-import discplot from "./discplot.js";
-import Interactions from "./discplotInteractions.js";
-import { buildBoostplot } from "./boostplot.js";
-
-
-console.log("main loaded")
